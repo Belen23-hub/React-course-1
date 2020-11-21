90c778345070a80e3d739840fd580c7d5a21e551
Belén menganita <belenbahmerrada@gmail.com>
2020-11-21 12:43:02 -0600

    aded event handlers

--- src/index.js
+++ src/index.js
@@ -35,12 +35,21 @@ function Booklist () {
 }
 
 const Book = ({img,title,author}) => {
+  const clickHandler = (e) =>{
+    console.log(e)
+    console.log(e.target)
+    alert('Hello ')
+  }
+  const complexExample = (author) => {
+    console.log(author);
+  }
   return (
-    <article className="book">
+    <article className="book" onMouseOver={()=> {console.log(title)}}>
     <img src={img} alt=''/>
-    <h1>{title}</h1>
+    <h1 onClick={() => console.log(title)}>{title}</h1>
     <h4>{author}</h4>
- 
+    <button type="buttom" onClick={ clickHandler}>Example</button>
+    <button type="button" onClick={() => complexExample(author)}>More complex example</button>
   </article>
   )
 }
