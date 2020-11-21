17580e10e9956b0297857070f1ff6d536b3e4b61
Belén menganita <belenbahmerrada@gmail.com>
2020-11-21 12:15:24 -0600

    done with props and spread operator

--- src/index.js
+++ src/index.js
@@ -2,32 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/71hKtvn3gXL._AC_UY218_.jpg",
-  title:'The Guardians: A Novel',
-  author: 'John Grisham',
-}
+const books = [
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/81XvmM2fYJL._AC_UY218_.jpg",
-  title:'Squeeze me: A Novel',
-  author: 'Carl Hiaasen',
-}
+  { id: 1,
+    img: "https://m.media-amazon.com/images/I/71hKtvn3gXL._AC_UY218_.jpg",
+    title:'The Guardians: A Novel',
+    author: 'John Grisham',
+},
+{   id: 2,
+    img: "https://m.media-amazon.com/images/I/81XvmM2fYJL._AC_UY218_.jpg",
+    title:'Squeeze me: A Novel',
+    author: 'Carl Hiaasen',
+},
+{   id: 3,
+    img: "https://m.media-amazon.com/images/I/71xgVzCVapL._AC_UY218_.jpg",
+    title:'It All Comes Back to You: A Book Club Recommendation!',
+    author: ' Beth Duke ',
+},
+];
+
+
 function Booklist () {
   return (
     <section className="booklist">
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+    {books.map((book) => {
+      return (
+        <Book key={book.id} {...book}></Book>
+      )
+    })}
     </section>
   );
 }
 
-const Book = (props) => {
+const Book = ({img,title,author}) => {
   return (
     <article className="book">
-    <img src={props.img} alt=''/>
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
+    <img src={img} alt=''/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+ 
   </article>
   )
 }
